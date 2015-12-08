2e0849ccd476c34c920fa5f311cec634d8d1430b
rex-master <Andrew@Andrews-MacBook-Air.local>
2015-12-07 21:43:19 -0500

    work

--- salestax.rb
+++ salestax.rb
@@ -1,8 +1,30 @@
 class Product
 	attr_accessor price, qty, name, type
+
+end
+
+class 
+	def intialize 
+		@productas = []
+end
+product
+@p
+class DiscProduct
+end
+
+class PaperProduct
 end
 
 class Cart
+	def initialize
+
 end
 
 candy = Product.new[1.50,1,"Canday","Food"]
+book = Product.new[12.50,10000000000000000000,"White Collar","Book"]
+movie = Product.new[26.00,2,"Nick Haldman","Movie"]
+
+cart = cart.new
+cart.add.product(canday)
+cart.add.product(book)
+
